3473057d92feb273f1606b74aab95fe0eadd1781
Sead Hadzibulic <89218691+SH328@users.noreply.github.com>
2022-01-04 18:49:25 -0600

    updated

---
+++ src/TestPractice.java
@@ -0,0 +1,11 @@
+public class TestPractice {
+
+    public static void main(String[] args) {
+
+        System.out.println("First");
+        System.out.println("Second");
+        System.out.println("Third");
+
+    }
+
+}
